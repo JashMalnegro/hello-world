14393d5036dd3147c56ba2d0cf0688005a0b2eb3
Jash Malnegro <Jashbeautiful@users.noreply.github.com>
2020-01-25 12:25:37 +0800

    flutter tutorial 3
    flutter tutorial part 3

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,24 +1,20 @@
 import "package:flutter/material.dart";
+import './app_screens/first_screen.dart';
 
-void main() {
+void main() => runApp(new MyFlutterApp());
 
-  runApp(
+class MyFlutterApp extends StatelessWidget {
 
-    MaterialApp(
-      title: "My Flutter App",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My First App Screen"),),
-        body: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
-              "Hello Flutter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-          ),
-        ),
-      )
-    )
-  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+          appBar: AppBar(title: Text("My First App Screen"),),
+          body: FirstScreen()
+        )
+    );
+  }
+
 }
